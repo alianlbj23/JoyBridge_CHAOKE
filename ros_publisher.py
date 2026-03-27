5ccf3f43823116bb784179465da5540afa6e26cb
alianlbj23 <alianlbj23@gmail.com>
2026-03-27 11:39:54 +0800

    Use Seanpai structure

--- ros_publisher.py
+++ ros_publisher.py
@@ -20,30 +20,43 @@ class RosPublisher:
         
         # 機械手臂發布器
         self.arm_publisher = None
+        self.arm_joint_names = []
+        self.arm_time_from_start_sec = 1
         if arm_config:
             self.arm_publisher = roslibpy.Topic(
                 client, 
                 arm_config['name'], 
                 arm_config['msg_type']
             )
+            self.arm_joint_names = arm_config.get('joint_names', [])
+            self.arm_time_from_start_sec = arm_config.get('time_from_start_sec', 1)
 
     def send_arm_joints(self, joint_angles_deg):
-        """發送機械手臂關節角度 (輸入為度，轉換為弧度發布)"""
+        """發送機械手臂 JointTrajectory (輸入為度，轉換為弧度發布)。"""
         if self.arm_publisher:
-            # 將角度從度轉換為弧度
             joint_angles_rad = [math.radians(angle) for angle in joint_angles_deg]
-            
-            # 創建 JointTrajectoryPoint 消息
+
             msg = {
-                'positions': joint_angles_rad,
-                'velocities': [],
-                'accelerations': [],
-                'effort': [],
-                'time_from_start': {'secs': 0, 'nsecs': 0}
+                'joint_names': self.arm_joint_names,
+                'points': [
+                    {
+                        'positions': joint_angles_rad,
+                        'velocities': [],
+                        'accelerations': [],
+                        'effort': [],
+                        'time_from_start': {
+                            'sec': self.arm_time_from_start_sec,
+                            'nanosec': 0
+                        }
+                    }
+                ]
             }
-            
+
             self.arm_publisher.publish(roslibpy.Message(msg))
-            print(f"[ARM] Published joint angles: {joint_angles_deg}° -> {joint_angles_rad} rad")
+            print(
+                f"[ARM] Published trajectory: joints={self.arm_joint_names}, "
+                f"angles={joint_angles_deg}° -> {joint_angles_rad} rad"
+            )
 
     def send(self, vec):
         for item in self.publishers:
